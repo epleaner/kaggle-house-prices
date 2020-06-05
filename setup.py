0c0cb34b265aae78dd0517af1af82c935e1e91de
Eli <eli.i.pleaner@gmail.com>
2020-06-05 15:55:57 +0200

    Initialize with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='repo for https://www.kaggle.com/c/house-prices-advanced-regression-techniques',
+    author='eli pleaner',
+    license='MIT',
+)
